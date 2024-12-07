47c189f0027ad8fd194c57dd05096f145ef5570d
Bloodimirg <bloodimirg@gmail.com>
2024-12-07 17:43:43 +0300

    init

--- net/apps.py
+++ net/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NetConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'net'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "net"
