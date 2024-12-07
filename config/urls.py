c3b9cee61319f26d5b4714eb4cc3caf93c2c4ce6
Bloodimirg <bloodimirg@gmail.com>
2024-12-07 17:50:24 +0300

    прописаны пути приложений и документации

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,10 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 
 
-
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
-        default_version='v1',
+        default_version="v1",
         description="Your API description",
         terms_of_service="https://www.example.com/policies/terms/",
         contact=openapi.Contact(email="contact@example.com"),
@@ -20,11 +19,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('net/', include("net.urls", namespace="net")),
+    path("admin/", admin.site.urls),
+    path("net/", include("net.urls", namespace="net")),
     path("users/", include("users.urls", namespace="users")),
-
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui",),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
