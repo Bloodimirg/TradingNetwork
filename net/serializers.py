b13f9138533080d0379f3cc754dfc076f4080e92
Bloodimirg <bloodimirg@gmail.com>
2024-12-07 17:46:45 +0300

    Добавлена валидация debt

--- net/serializers.py
+++ net/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from .models import Supplier, Product
 
+
 class SupplierSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        # Проверяем, не пытались ли изменить задолженность
+        if "debt" in data:
+            raise serializers.ValidationError(
+                {"debt": "Невозможно изменить задолженность перед поставщиком."}
+            )
+        return data
+
     class Meta:
         model = Supplier
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
