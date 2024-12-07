68a3934807d697363bb392695c33ee7929f6f89e
Bloodimirg <bloodimirg@gmail.com>
2024-12-07 17:45:40 +0300

    PEP8

--- net/models.py
+++ net/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models import SET_NULL
 from users.models import NULLABLE
-from .utils import save_supplier
 
 
 class Supplier(models.Model):
@@ -24,21 +23,28 @@ class Supplier(models.Model):
     house_number = models.PositiveIntegerField(verbose_name="Номер дома")
 
     level = models.PositiveSmallIntegerField(choices=LEVEL, verbose_name="Уровень сети")
-    supplier = models.ForeignKey("self",  on_delete=SET_NULL, related_name="suppliers", verbose_name="Поставщик", **NULLABLE)
-    debt = models.DecimalField(max_digits=15, decimal_places=2, default=0.00, verbose_name="Задолженность перед поставщиком руб.")
+    supplier = models.ForeignKey(
+        "self",
+        on_delete=SET_NULL,
+        related_name="suppliers",
+        verbose_name="Поставщик",
+        **NULLABLE,
+    )
+    debt = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        default=0.00,
+        verbose_name="Задолженность перед поставщиком руб.",
+    )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-
-    def save(self, *args, **kwargs):
-        # Вызываем переопределенную функцию для вычисления уровня и сохранения данных
-        save_supplier(self, *args, **kwargs)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Звено сети"
-        verbose_name_plural = "Звенья сети"
+        verbose_name = "Поставщик"
+        verbose_name_plural = "Поставщики"
+
 
 class Product(models.Model):
     """Модель продукта"""
@@ -47,9 +53,10 @@ class Product(models.Model):
     product_model = models.CharField(max_length=250, verbose_name="Модель продукта")
     release_date = models.DateField(verbose_name="Дата выхода продукта на рынок")
     supplier = models.ForeignKey(
-        Supplier, on_delete=models.CASCADE, verbose_name="Поставщик"
+        Supplier,
+        on_delete=models.CASCADE,
+        verbose_name="Поставщик"
     )
-
 
     def __str__(self):
         return f"{self.name} ({self.product_model})"
@@ -57,4 +64,3 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
-
